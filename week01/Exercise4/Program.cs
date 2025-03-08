2bd01bebad7d13f524847e910867efde74183203
prestonakagi <preston.akagi@gmail.com>
2025-03-08 15:39:14 -0700

    week 01 exercises 4 and 5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        /* lesson said put "using System.Collections.Generic" at top of script
+        whenever use List or other standard collection. 
+        HOWEVER, the sample solution does not have that, so assume the environment
+        is set up to already have that. */
+
+        List<float> numbers = new List<float>();
+
+        float userNumber = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            userNumber = float.Parse(userInput);
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        float total = 0;
+        foreach (float number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The sum is: {total}");
+
+        int countOfNumbers = numbers.Count;
+        float average = total / countOfNumbers;
+        Console.WriteLine($"The average is: {average}");
+
+        // find maxiumum number
+        float largest = -1000000000000000000;
+        for (int i = 1; i < countOfNumbers; i++)
+        {
+            if (numbers[i] >= largest)
+            {
+                largest = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // static for "regular"/non-Class function.
+        // function general structure:
+        /* static returnType FunctionName(dtype parameter1, dtype parameter2)
+           {
+                function_body
+           }
+        */
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");            
+        }
+        
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite integer number: ");
+            string userNumber = Console.ReadLine();
+            int number = int.Parse(userNumber);
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            /* could use Math.Pow() like following:
+            int squared = (int)Math.Pow(number, 2);
+            the (int)Ma... part "casts the result from a double value to int".
+            */
+            return squared;
+        }
+
+        static void DisplayResult(string name, int squared)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squared}");
+        }
+
+        static void Main()
+        {
+            DisplayWelcome();
+
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+            int squared = SquareNumber(number);
+
+            DisplayResult(name, squared);
+        }
+
+        Main();
     }
 }
