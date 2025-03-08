f88c185abc24de0bf36e0d008367d01d35fd7dca
prestonakagi <preston.akagi@gmail.com>
2025-03-08 00:51:41 -0700

    upload my notes for Csharp fundamentals. Also week01 exercises 1 to 3.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string stringGrade = Console.ReadLine();
+        float grade = float.Parse(stringGrade);
+        string letter = "Z";
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+            Console.WriteLine($"Your grade is a {letter}");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class! Congratulations!");
+        }
+        else if (grade < 70)
+        {
+            Console.WriteLine("You did not pass the class. But you will do better next time.");
+        }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,34 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    {        
+        Random randomNumber = new Random(); // create instance of Random class.
+        int magicNumber = randomNumber.Next(1, 101); // generate random int between 1 and 100.
+        int guess = 0;
+        // need initiate variables before while loop and maybe for do while loop too.
+        
+        while (guess != magicNumber)
+        {
+            /*Console.Write("What is the magic number? ");
+            string stringMagicNumber = Console.ReadLine();
+            magicNumber = int.Parse(stringMagicNumber);*/
+
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            guess = int.Parse(userGuess);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
