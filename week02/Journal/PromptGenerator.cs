82a185c96cef6c52df4b0ff1e38549bbdc11169a
prestonakagi <preston.akagi@gmail.com>
2025-03-13 02:24:01 -0600

    Journal project class diagram and stubbed out classes' code.

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,11 @@
+public class PromptGenerator
+{
+    public List<string> _prompts;
+
+    public string GetRandomPrompt()
+    {
+        // when stub out code, got/get error "not all code paths return a value".
+        // So write a return value just to have one, until write the actual code, so program can compile.
+        return "";
+    }    
+}
