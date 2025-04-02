5f43780714d877a6791415a1ec5c6816e8f26fb4
prestonakagi <preston.akagi@gmail.com>
2025-04-02 01:13:40 -0600

    Inheritance learning activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment testAssign = new Assignment("Samuel Bennett", "Multiplication");
+
+        Console.WriteLine($"{testAssign.GetSummary()}");
+
+        MathAssignment testMath = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+
+        Console.WriteLine($"{testMath.GetSummary()}\n{testMath.GetHomeworkList()}");
+
+        WritingAssignment testWriting = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine($"{testWriting.GetSummary()}\n{testWriting.GetWritingInformation()}");
     }
 }
